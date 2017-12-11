79976cafa4db5ea8a8f9f596083261b6522623d4
Jorge14288 <jorge.fuentes.14288@gmail.com>
2017-12-11 09:49:09 -0600

    first commit

---
+++ src/pages/encriptacion/encriptacion.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { EncriptacionPage } from './encriptacion';
+
+@NgModule({
+  declarations: [
+    EncriptacionPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(EncriptacionPage),
+  ],
+})
+export class EncriptacionPageModule {}
